f3c971bf72cfc1ee5d0eda344730c18311b52382
Socks <patricksutcliffe123@gmail.com>
2021-10-18 23:25:39 -0400

    work on the seeds.sql

---
+++ db/seeds.sql
@@ -0,0 +1,32 @@
+INSERT INTO department (name)
+VALUES  ('Marketing'),
+        ('Finance'),
+        ('Operations'),
+        ('Human Resource'),
+        ('IT');
+
+INSERT INTO roles (title, salary, department_id)
+VALUES  ('Marketing analyst', 200000, 1),
+        ('Marketing consultant', 120000, 1),
+        ('Accountant', 130000, 2),
+        ('Financial Agent', 100000, 2),
+        ('Analyst', 120000, 3),
+        ('Program Manager', 20000, 3),
+        ('Recruiting', 140000, 4),
+        ('Director', 210,000, 4),
+        ('Systems Engineer', 200000, 5),
+        ('Help Desk', 120000, 5);
+
+
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
+VALUES  ('Patrick','Sutcliffe', 9, 2),
+        ('Daniel', 'Kim', 5, 2),
+        ('Josh', 'Watkins', 1, 2),
+        ('Bradley', 'Kimbrell', 2, 4),
+        ('Caitlin', 'Wall', 4, 2),
+        ('Morgan', 'Riley', 3, 2),
+        ('Matt', 'Gomez', 6, 2),
+        ('Zac', 'Goad', 8, 2),
+        ('Abraham','Hong', 10, 2),
+        ('Courtney', 'Long', 7, 2);
+
