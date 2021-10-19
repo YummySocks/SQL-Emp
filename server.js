84005720ddd2c92ebdf9e714a378952861e62f8f
Socks <patricksutcliffe123@gmail.com>
2021-10-19 10:43:18 -0400

    added questions and got them exported properly

--- server.js
+++ server.js
@@ -1,3 +1,13 @@
 const cTable = require('console.table');
 const inquirer = require('inquirer');
 const mysql = require('mysql2');
+const { main, 
+        addEmp, 
+        roleEmp, 
+        manageEmp, 
+        upEmp, 
+        addRole, 
+        roleDep,
+        addDep} = require('./lib/questions')
+
+inquirer.prompt(main)
